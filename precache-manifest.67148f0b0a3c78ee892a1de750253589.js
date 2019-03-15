a78cd54799bdb5d41dfc9b90720daa6c28fa030d
Shherbakova <s.shherbakova@yandex.ru>
2019-03-15 11:31:09 +0300

    Updates

---
+++ precache-manifest.67148f0b0a3c78ee892a1de750253589.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b46d7be5431830175ab",
+    "url": "/myyyy/static/js/runtime~main.5b46d7be.js"
+  },
+  {
+    "revision": "0db0a888414f9857edb5",
+    "url": "/myyyy/static/js/main.0db0a888.chunk.js"
+  },
+  {
+    "revision": "44df2f2ad171bf772123",
+    "url": "/myyyy/static/js/2.44df2f2a.chunk.js"
+  },
+  {
+    "revision": "0db0a888414f9857edb5",
+    "url": "/myyyy/static/css/main.58099ec0.chunk.css"
+  },
+  {
+    "revision": "3a51745d578fdb082ccc8a6405cc6cc5",
+    "url": "/myyyy/index.html"
+  }
+];
